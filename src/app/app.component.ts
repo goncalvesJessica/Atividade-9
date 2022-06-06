10ef26f6663ff72f6c37b86ac1fcde0668a307b1
goncalvesJessica <jessica.tayane2014@gmail.com>
2022-06-06 14:22:53 -0300

    tarefa9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Cadastro-Estudantes';
-  
+  title = 'Cadastro Estudantes';
+
 }
